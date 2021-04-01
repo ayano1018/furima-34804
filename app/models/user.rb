cf274a550c34f25329385b92dea7612aac85f456
ayano1018 <ayanolife@gmail.com>
2021-04-01 14:52:52 +0900

    with_optionsでまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,18 @@ class User < ApplicationRecord
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :nickname, presence: true, length:  { maximum: 40}
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です' }
+ with_options presence: true do
+  validates :nickname, length:  { maximum: 40}
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です' }
   validates :Password, presence: true, format: { with:  VALID_PASSWORD_VALIDATION, message: 'は英字と数字の両方を含めて設定してください' }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+ end
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
   validates :first_name
   validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "カタカナを使用してください"}do
+with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "カタカナを使用してください"}do
   validates :last_name_kana
   validates :first_name_kana 
   end
   validates :birth, presence: true
-  end
+end
