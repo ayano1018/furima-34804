b7ab4b256ee083f3c0c2c49885616ea8394baad8
ayano1018 <ayanolife@gmail.com>
2021-04-01 14:47:46 +0900

    エラー修正（パスワードの記入変更）

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
 
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_VALIDATION = /\A[\w\-]+\z/
+  VALID_PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :nickname, presence: true, length:  { maximum: 40}
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です' }
-  validates :password_confirmation, presence: true, format: { with:  VALID_PASSWORD_VALIDATION, message: 'は英字と数字の両方を含めて設定してください' }
+  validates :Password, presence: true, format: { with:  VALID_PASSWORD_VALIDATION, message: 'は英字と数字の両方を含めて設定してください' }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
   validates :first_name
   validates :last_name
