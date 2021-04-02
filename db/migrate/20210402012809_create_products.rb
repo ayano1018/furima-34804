7ba732ab749886eef855ee7f2f53b77e6b499cbb
ayano1018 <ayanolife@gmail.com>
2021-04-02 11:01:00 +0900

    商品出品機能テーブル作成

---
+++ db/migrate/20210402012809_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string   :name,              null: false
+      t.text     :description,       null: false
+      t.integer  :category_id,       null: false  
+      t.integer  :condition_id,      null: false
+      t.integer  :shipping_cost_id,  null: false
+      t.integer  :region_id,         null: false
+      t.integer  :shipping_day_id,   null: false
+      t.integer  :price,             null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_021352) do
+ActiveRecord::Schema.define(version: 2021_04_02_012809) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "region_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
