1bfd295728af4cf4807735fbfe0959477d10a1eb
ayano1018 <ayanolife@gmail.com>
2021-04-03 16:54:56 +0900

    単体テストコード

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :product do
-    
-  end
+      name                   {"犬ロボット"}
+      description            {"中型犬のサイズです"}
+      category_id            {6}
+      condition_id           {"1"}
+      region_id              {"13"}
+      shipping_cost_id       {"1"}
+      shipping_day_id        {"2"}
+      price                  {"299"}
+      association :user
+  end  
 end
