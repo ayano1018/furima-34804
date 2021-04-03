f412006d3fb14902fe6315055c4421762c4669b7
ayano1018 <ayanolife@gmail.com>
2021-04-03 18:39:54 +0900

    バリデーションwith_options記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,15 +10,16 @@ extend ActiveHash::Associations::ActiveRecordExtensions
  belongs_to_active_hash  :shipping_cost
  belongs_to_active_hash  :shipping_day
 
- validates :name,             presence: true
- validates :description,      presence: true
- validates :category_id,      presence: true
- validates :condition_id,     presence: true
- validates :shipping_cost_id, presence: true
- validates :region_id,        presence: true
- validates :shipping_day_id,  presence: true
- validates :price,            presence: true,  numericality: {greater_than: 300, less_than: 10000000} 
-
+ with_options presence: true do
+  validates :name,             presence: true
+  validates :description,      presence: true
+  validates :category_id,      presence: true
+  validates :condition_id,     presence: true
+  validates :shipping_cost_id, presence: true
+  validates :region_id,        presence: true
+  validates :shipping_day_id,  presence: true
+  validates :price,            presence: true,  numericality: {greater_than: 300, less_than: 10000000} 
+ end
 
 
 end
